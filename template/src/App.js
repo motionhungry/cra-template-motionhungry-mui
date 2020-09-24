ec0e5b5bca41372923d82ff065a0da5203e77598
Kyle Getrost <kyle@motionhungry.com>
2020-09-24 00:34:01 -0700

    feat: add theme support

--- template/src/App.js
+++ template/src/App.js
@@ -5,24 +5,28 @@ import {
   Route,
 } from "react-router-dom";
 import CssBaseline from '@material-ui/core/CssBaseline';
+import { ThemeProvider } from '@material-ui/core/styles';
 
 import NavBar from './components/NavBar';
+import routes from './config/routes';
+import theme from './config/theme';
 import About from './pages/About';
 import Home from './pages/Home';
-import routes from './config/routes';
 
 const App = () => (
   <Router>
-    <CssBaseline />
-    <NavBar />
-    <Switch>
-      <Route path={routes.about}>
-        <About />
-      </Route>
-      <Route path={routes.home}>
-        <Home />
-      </Route>
-    </Switch>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <NavBar />
+      <Switch>
+        <Route path={routes.about}>
+          <About />
+        </Route>
+        <Route path={routes.home}>
+          <Home />
+        </Route>
+      </Switch>
+    </ThemeProvider>
   </Router>
 );
 
